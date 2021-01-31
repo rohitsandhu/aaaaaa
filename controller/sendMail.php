1fdfa79dbba31072d24f59322ee6ca934843cc6d
rohitsandhu <xxmdluffxx@gmail.com>
2021-01-31 16:18:49 +0100

    segon

--- controller/sendMail.php
+++ controller/sendMail.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 require '../phpmailer/src/Exception.php';
 require '../phpmailer/src/PHPMailer.php';
 require '../phpmailer/src/SMTP.php';
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
 
 
 function sendMail($user){
